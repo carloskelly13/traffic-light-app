d738e111a2cdde4bede04bfa25a59fd6b2dde7f3
Carlos Kelly <me@carlos.dev>
2023-07-26 21:20:50 -0500

    Rename relay gpio flags to better reflect their physical state.

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -4,28 +4,25 @@ const redPin = new Gpio(26, "out")
 const yellowPin = new Gpio(20, "out")
 const greenPin = new Gpio(21, "out")
 
-/**
- * The GPIO pins on the Raspberry pins take a 0 signal to close the relay on
- * and a 0 signal to open the relay.
- */
-const ON = 0
-const OFF = 1
+const CLOSED = 0
+const OPEN = 1
 
 const delay = (time: number) =>
   new Promise(resolve => setTimeout(resolve, time))
 
 const reset = () =>
   Promise.allSettled([
-    greenPin.write(OFF),
-    yellowPin.write(OFF),
-    redPin.write(OFF),
+    greenPin.write(OPEN),
+    yellowPin.write(OPEN),
+    redPin.write(OPEN),
   ])
 
 const main = async () => {
   try {
     await reset()
-    await greenPin.write(ON)
+    await greenPin.write(CLOSED)
     await delay(3000)
+    await greenPin.write(OPEN)
   } catch (e) {
     console.error(e)
   }
