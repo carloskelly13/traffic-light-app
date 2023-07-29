7b16d3c6874f520a59cb100da202b14ff1e99699
Carlos Kelly <me@carlos.dev>
2023-07-29 13:50:19 -0500

    Add Vercel build

---
+++ packages/app/api/ssr.ts
@@ -0,0 +1,22 @@
+import { renderPage } from "vite-plugin-ssr"
+import type { VercelRequest, VercelResponse } from "@vercel/node"
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const { url } = req
+  if (url === undefined) throw new Error("req.url is undefined")
+
+  const pageContextInit = { url }
+  const pageContext = await renderPage(pageContextInit)
+  const { httpResponse } = pageContext
+
+  if (!httpResponse) {
+    res.statusCode = 200
+    res.end()
+    return
+  }
+
+  const { body, statusCode, contentType } = httpResponse
+  res.statusCode = statusCode
+  res.setHeader("content-type", contentType)
+  res.end(body)
+}
--- packages/app/pages/index/index.page.tsx
+++ packages/app/pages/index/index.page.tsx
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Page = (): JSX.Element => {
+  const [counter, setCounter] = useState(0)
   return (
     <>
       <h1>Carlos App!</h1>
       This page is generated using Vite SSR plugin and on AWS
+      {counter}
+      <button onClick={() => setCounter(counter + 1)}>Add!</button>
     </>
   )
 }
