c22f142f54216670274b89f754e2a1b4f20f3092
Carlos Kelly <me@carlos.dev>
2023-07-30 11:58:28 -0500

    Add security around signal endpoint

--- packages/app/app/routes/signal/index.ts
+++ packages/app/app/routes/signal/index.ts
@@ -2,7 +2,13 @@ import type { LoaderArgs } from "@remix-run/node"
 import { json } from "@remix-run/node"
 import Pusher from "pusher"
 
-const { PUSHER_APP_ID, PUSHER_KEY, PUSHER_SECRET, PUSHER_CLUSTER } = process.env
+const {
+  PUSHER_APP_ID,
+  PUSHER_KEY,
+  PUSHER_SECRET,
+  PUSHER_CLUSTER,
+  API_ACCESS_TOKEN,
+} = process.env
 
 const pusher = new Pusher({
   appId: PUSHER_APP_ID || "",
@@ -13,6 +19,11 @@ const pusher = new Pusher({
 })
 
 export const loader = async ({ request }: LoaderArgs) => {
+  const headerApiAccessToken = request.headers.get("api-access-token")
+  if (!headerApiAccessToken || headerApiAccessToken !== API_ACCESS_TOKEN) {
+    return json("Unauthorized", 401)
+  }
+
   try {
     const response = await pusher.trigger("traffic-light-channel", "signal", {
       message: "hello world",
